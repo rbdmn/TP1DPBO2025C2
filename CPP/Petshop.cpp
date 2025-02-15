a1c841e76ffaabd974754a939f52f547d44e0377
rbdmn <raufbudiman@gmail.com>
2025-02-15 21:04:53 +0700

    FIX

--- CPP/Petshop.cpp
+++ CPP/Petshop.cpp
@@ -1,84 +1,104 @@
+// Library
 #include <iostream>
 #include <string>
 #include <list>
 
 using namespace std;
 
+// CLASS PETSHOP
 class Petshop
 {
-    private:
-        int id;
-        string nama_produk;
-        string kategori_produk;
-        double harga_produk;
+    
+private: // Isi variabel-variabel dengan akses terprivate (tertutup)
+    int id;                 // Variabel ID untuk identitas produk
+    string nama_produk;     // Variabel nama produk untuk nama pada produk
+    string kategori_produk; // Variabel kategori produk untuk kategori pada produk
+    double harga_produk;    // Variabel harga produk untuk harga pada produk
 
-    public:
-        Petshop()
-        {
-            this->id = 0;
-            this->nama_produk = "";
-            this->kategori_produk = "";
-            this->harga_produk = 0.0;
-        }
+public: // Isi dengan akses public (terbuka)
 
-        Petshop(int id, string nama_produk, string kategori_produk, double harga_produk)
-        {
-            this->id = id;
-            this->nama_produk = nama_produk;
-            this->kategori_produk = kategori_produk;
-            this->harga_produk = harga_produk;
-        }
+    // [CONSTRUCTOR] == PEMBUKA
 
-        int get_id()
-        {
-            return this->id;
-        }
-        
-        void set_id(int id)
-        {
-            this->id = id;
-        }
+    Petshop() // Default Constructor untuk inisialisasi dengan nilai kosong
+    {
+        // Semua isi dari tiap variabel di kosongkan
+        this->id = 0;
+        this->nama_produk = "";
+        this->kategori_produk = "";
+        this->harga_produk = 0.0;
+    }
 
-        string get_nama_produk()
-        {
-            return this->nama_produk;
-        }
-        
-        void set_nama_produk(string nama_produk)
-        {
-            this->nama_produk = nama_produk;
-        }
+    Petshop(int id, string nama_produk, string kategori_produk, double harga_produk) // Constructor untuk memberikan nilai awal pada variabel (makanya pake parameter)
+    {
+        // Semua isi dari tiap variabel di berikan nilainya sesuai dengan parameternya
+        this->id = id;
+        this->nama_produk = nama_produk;
+        this->kategori_produk = kategori_produk;
+        this->harga_produk = harga_produk;
+    }
 
-        string get_kategori_produk()
-        {
-            return this->kategori_produk;
-        }
-        
-        void set_kategori_produk(string kategori_produk)
-        {
-            this->kategori_produk = kategori_produk;
-        }
+    // [METHODS] == ACTION PADA CLASS
 
-        double get_harga_produk()
-        {
-            return this->harga_produk;
-        }
-        
-        void set_harga_produk(double harga_produk)
-        {
-            this->harga_produk = harga_produk;
-        }
+    // Dapatkan value dari variabel ID
+    int get_id()
+    {
+        return this->id;
+    }
 
-        void tabelDatabase()
-        {
-            cout << "\n|ID: " << this->id << "|\n";
-            cout << "|Nama Produk: " << this->nama_produk << "|\n";
-            cout << "|Kategori Produk: " << this->kategori_produk << "|\n";
-            cout << "|Harga Produk: " << this->harga_produk << "|\n";
-        }
+    // Atur value dari variabel ID
+    void set_id(int id)
+    {
+        this->id = id;
+    }
 
-        ~Petshop()
-        {
-            
-        }
+    // Dapatkan value dari variabel nama produk
+    string get_nama_produk()
+    {
+        return this->nama_produk;
+    }
+
+    // Atur value dari variabel nama produk
+    void set_nama_produk(string nama_produk)
+    {
+        this->nama_produk = nama_produk;
+    }
+
+    // Dapatkan value dari variabel kategori produk
+    string get_kategori_produk()
+    {
+        return this->kategori_produk;
+    }
+
+    // Atur value dari variabel kategori produk
+    void set_kategori_produk(string kategori_produk)
+    {
+        this->kategori_produk = kategori_produk;
+    }
+
+    // Dapatkan value dari variabel harga produk
+    double get_harga_produk()
+    {
+        return this->harga_produk;
+    }
+
+    // Atur value dari variabel haga produk
+    void set_harga_produk(double harga_produk)
+    {
+        this->harga_produk = harga_produk;
+    }
+
+    // method untuk menampilkan isi dari database, bersifat tabel
+    void tabelDatabase()
+    {
+        cout << "\n|ID: " << this->id << "|\n";
+        cout << "|Nama Produk: " << this->nama_produk << "|\n";
+        cout << "|Kategori Produk: " << this->kategori_produk << "|\n";
+        cout << "|Harga Produk: " << this->harga_produk << "|\n";
+    }
+
+    // [DESTRUCTOR] == PENUTUP
+
+    ~Petshop()
+    {
+    }
 };
--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,9 +1,13 @@
+// Library
 #include <iostream>
 #include <list>
+
+// Import petshop.cpp untuk definisi class
 #include "Petshop.cpp"
 
 using namespace std;
 
+// fungsi untuk menampilkan menu pilihan
 void tampilkan_menu()
 {
     cout << "\n@=@=====================================@\n";
@@ -18,32 +22,39 @@ void tampilkan_menu()
     cout << "@=======================================@\n";
 }
 
-void tampilkan_data(list<Petshop> &llist)
+// fungsi untuk menampilkan data nya
+void tampil_data(list<Petshop> &llist)
 {
-    if (llist.empty())
+    if (llist.empty()) // jika isi pada list kosong
     {
-        cout << "Data belum terisi cuy\n";
+        cout << "Data belum terisi cuy\n"; // error handling
     }
-    else
+    else // jika tidak kosong
     {
+        // maka tampilkan datanya
         cout << "Data yang tersedia:\n";
         int i = 0;
+
+        // iterasi tiap elemen pada list menggunakan semacam iterator
         for (list<Petshop>::iterator it = llist.begin(); it != llist.end(); it++)
         {
-            cout << (i + 1) << ". ";
-            it->tabelDatabase();
-            cout << '\n';
-            i++;
+            cout << (i + 1) << ". "; // outputkan nomor urutan pada data di list
+            it->tabelDatabase(); // panggil tabelDatabase, yang isinya outputan/isi dari list tersebut
+            cout << '\n'; // outputkan newline sebagai spasi baru
+            i++; // i beriterasi
         }
     }
 }
 
+// fungsi untuk menambahkan data nya
 void tambah_data(list<Petshop> &llist)
 {
+    // variabel variabel yang dipakai
     int id;
     string nama_produk, kategori_produk;
     double harga_produk;
 
+    // masukan sesuai atribut masing masing
     cout << "Masukkan ID: ";
     cin >> id;
     cout << "Masukkan Nama Produk: ";
@@ -53,30 +64,37 @@ void tambah_data(list<Petshop> &llist)
     cout << "Masukkan Harga Produk: ";
     cin >> harga_produk;
 
+    // buatkan suatu temp untuk menampung dan menambahkan ke list
     Petshop temp;
 
+    // temp tersebut di set/atur value nya dengan inputan yang sudah user masukan
     temp.set_id(id);
     temp.set_nama_produk(nama_produk);
     temp.set_kategori_produk(kategori_produk);
     temp.set_harga_produk(harga_produk);
 
+    // lalu masukan value value pada temp ke list dengan cara push back
     llist.push_back(temp);
     cout << "Yey, Data berhasil ditambahkan!\n";
 }
 
+// fungsi untuk mengubah data
 void ubah_data(list<Petshop> &llist)
 {
-    int id;
+    int id; // variabel id
     cout << "Masukkan ID data yang ingin diubah: ";
-    cin >> id;
+    cin >> id; // inputkan id
 
+    // iterasi tiap elemen pada list menggunakan semacam iterator
     for (list<Petshop>::iterator it = llist.begin(); it != llist.end(); it++)
     {
-        if (it->get_id() == id)
+        if (it->get_id() == id) // jika ketemu id nya pada list
         {
+            // atribut-atribut lainnya
             string nama_produk, kategori_produk;
             double harga_produk;
 
+            // masukkan dan inputkan variabel/atribut2 tersebut
             cout << "Masukkan Nama Produk Baru: ";
             cin >> nama_produk;
             cout << "Masukkan Kategori Produk Baru: ";
@@ -84,6 +102,7 @@ void ubah_data(list<Petshop> &llist)
             cout << "Masukkan Harga Produk Baru: ";
             cin >> harga_produk;
 
+            // lalu ganti data yang dulu menjadi yang baru. gunakan set untuk mengubah value nya
             it->set_nama_produk(nama_produk);
             it->set_kategori_produk(kategori_produk);
             it->set_harga_produk(harga_produk);
@@ -92,80 +111,91 @@ void ubah_data(list<Petshop> &llist)
             return;
         }
     }
+
+    // jika tidak ketemu data ID yang tepat
     cout << "Data dengan ID " << id << " tidak ditemukan!\n";
 }
 
+// fungsi untuk menghapus data
 void hapus_data(list<Petshop> &llist)
 {
-    int id;
+    int id; // variabel/atribut id
     cout << "Masukkan ID data yang mau dihapus: ";
-    cin >> id;
+    cin >> id; // inputkan id
 
+    // iterasi tiap elemen pada list menggunakan semacam iterator
     for (list<Petshop>::iterator it = llist.begin(); it != llist.end(); it++)
     {
-        if (it->get_id() == id)
+        if (it->get_id() == id) // jika ketemu id nya pada list
         {
-            llist.erase(it);
+            llist.erase(it); // hapus data tersebut dengan erase
             cout << "Data berhasil dihapus!\n";
             return;
         }
     }
+
+    // jika tidak ditemukan id nya
     cout << "Data dengan ID " << id << " tidak ditemukan!\n";
 }
 
+// fungsi untuk mencari data berdasarkan nama produk
 void cari_data(list<Petshop> &llist)
 {
+    // inisialisasikan nama produk
     string nama_produk;
     cout << "Masukkan Nama Produk yang ingin dicari: ";
-    cin >> nama_produk;
+    cin >> nama_produk; // input nama produk
 
+    // iterasi tiap elemen pada list menggunakan semacam iterator
     for (list<Petshop>::iterator it = llist.begin(); it != llist.end(); it++)
     {
-        if (it->get_nama_produk() == nama_produk)
+        if (it->get_nama_produk() == nama_produk) // jika data ditemukan sesuai dengan input (gunakan get untuk mendapatkan value nama produk)
         {
             cout << "Data ditemukan:\n";
-            it->tabelDatabase();
+            it->tabelDatabase(); // tampilkan tabel database
             return;
         }
     }
+
+    // jika nama produk tidak ditemukan
     cout << "Data dengan nama produk " << nama_produk << " tidak ditemukan!\n";
 }
 
 int main()
 {
-    list<Petshop> llist;
-    int pilihanMenu;
+    list<Petshop> llist; // inisialisasikan list
+    int pilihanMenu; // variabel untuk pilihan menunya
 
     do
     {
-        tampilkan_menu();
+        tampilkan_menu(); // tampilkan menunya terlebih dahulu
         cout << "Pilih menu berapa: ";
-        cin >> pilihanMenu;
+        cin >> pilihanMenu; // inputkan pilihan menu
 
         switch (pilihanMenu)
         {
-        case 1:
-            tampilkan_data(llist);
+        case 1: // case 1: tampilkan data
+            tampil_data(llist);
             break;
-        case 2:
+        case 2: // case 2: tambahkan data
             tambah_data(llist);
             break;
-        case 3:
+        case 3: // case 3: ubah data
             ubah_data(llist);
             break;
-        case 4:
+        case 4: // case 4: hapus data
             hapus_data(llist);
             break;
-        case 5:
+        case 5: // case 5: cari data berdasarkan nama produk
             cari_data(llist);
             break;
-        case 6:
+        case 6: // case 6: keluar dari program
             cout << "BAY BAY\n";
             break;
-        default:
+        default: // case selain 1-6: tampilkan error handling
             cout << "gaada menu no " << pilihanMenu << "\n";
         }
-    } while (pilihanMenu != 6);
+    } while (pilihanMenu != 6); // looping terus program sampai nomor 6 untuk exit
 
     return 0;
 }
